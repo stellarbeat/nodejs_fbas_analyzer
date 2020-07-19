bf7606c2f8f1f4706b587d23a1d4fcef1811b919
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-07-19 12:06:28 +0200

    refactoring & code cleanup

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -118,57 +118,14 @@ declare_types! {
             let js_has_quorum_intersection = cx.boolean(analysis_result.has_quorum_intersection);
             let js_has_quorum_intersection_faulty_nodes_filtered = cx.boolean(analysis_result.has_quorum_intersection_faulty_nodes_filtered);
 
-            let js_minimal_blocking_sets = JsArray::new(&mut cx, analysis_result.minimal_blocking_sets.len() as u32);
-            for (i, minimal_blocking_set) in analysis_result.minimal_blocking_sets.iter().enumerate() {
-                let js_minimal_blocking_set = JsArray::new(&mut cx, minimal_blocking_set.len() as u32);
-                for (i, node_public_key) in minimal_blocking_set.iter().enumerate() {
-                    let js_node_public_key = cx.string(node_public_key);
-                    js_minimal_blocking_set.set(&mut cx, i as u32, js_node_public_key).unwrap();
-                }
-                js_minimal_blocking_sets.set(&mut cx, i as u32, js_minimal_blocking_set).unwrap();
-            }
+            let js_minimal_blocking_sets = vec_vec_to_js_array_array(&mut cx, analysis_result.minimal_blocking_sets.clone());
+            let js_minimal_blocking_sets_faulty_nodes_filtered = vec_vec_to_js_array_array(&mut cx, analysis_result.minimal_blocking_sets_faulty_nodes_filtered.clone());
 
-            let js_minimal_blocking_sets_faulty_nodes_filtered = JsArray::new(&mut cx, analysis_result.minimal_blocking_sets_faulty_nodes_filtered.len() as u32);
-            for (i, minimal_blocking_set) in analysis_result.minimal_blocking_sets.iter().enumerate() {
-                let js_minimal_blocking_set = JsArray::new(&mut cx, minimal_blocking_set.len() as u32);
-                for (i, node_public_key) in minimal_blocking_set.iter().enumerate() {
-                    let js_node_public_key = cx.string(node_public_key);
-                    js_minimal_blocking_set.set(&mut cx, i as u32, js_node_public_key).unwrap();
-                }
-                js_minimal_blocking_sets_faulty_nodes_filtered.set(&mut cx, i as u32, js_minimal_blocking_set).unwrap();
-            }
+            let js_minimal_splitting_sets = vec_vec_to_js_array_array(&mut cx, analysis_result.minimal_splitting_sets.clone());
+            let js_minimal_splitting_sets_faulty_nodes_filtered = vec_vec_to_js_array_array(&mut cx, analysis_result.minimal_splitting_sets_faulty_nodes_filtered.clone());
 
-            let js_minimal_splitting_sets = JsArray::new(&mut cx, analysis_result.minimal_splitting_sets.len() as u32);
-            for (i, minimal_splitting_set) in analysis_result.minimal_splitting_sets.iter().enumerate() {
-                let js_minimal_splitting_set = JsArray::new(&mut cx, minimal_splitting_set.len() as u32);
-                for (i, node_public_key) in minimal_splitting_set.iter().enumerate() {
-                    let js_node_public_key = cx.string(node_public_key);
-                    js_minimal_splitting_set.set(&mut cx, i as u32, js_node_public_key).unwrap();
-                }
-                js_minimal_splitting_sets.set(&mut cx, i as u32, js_minimal_splitting_set).unwrap();
-            }
-
-            let js_minimal_splitting_sets_faulty_nodes_filtered = JsArray::new(&mut cx, analysis_result.minimal_splitting_sets_faulty_nodes_filtered.len() as u32);
-            for (i, minimal_splitting_set) in analysis_result.minimal_splitting_sets_faulty_nodes_filtered.iter().enumerate() {
-                let js_minimal_splitting_set = JsArray::new(&mut cx, minimal_splitting_set.len() as u32);
-                for (i, node_public_key) in minimal_splitting_set.iter().enumerate() {
-                    let js_node_public_key = cx.string(node_public_key);
-                    js_minimal_splitting_set.set(&mut cx, i as u32, js_node_public_key).unwrap();
-                }
-                js_minimal_splitting_sets_faulty_nodes_filtered.set(&mut cx, i as u32, js_minimal_splitting_set).unwrap();
-            }
-
-            let js_top_tier = JsArray::new(&mut cx, analysis_result.top_tier.len() as u32);
-            for (i, node_public_key) in analysis_result.top_tier.iter().enumerate() {
-                    let js_node_public_key = cx.string(node_public_key);
-                    js_top_tier.set(&mut cx, i as u32, js_node_public_key).unwrap();
-            }
-
-            let js_top_tier_faulty_nodes_filtered = JsArray::new(&mut cx, analysis_result.top_tier_faulty_nodes_filtered.len() as u32);
-            for (i, node_public_key) in analysis_result.top_tier_faulty_nodes_filtered.iter().enumerate() {
-                    let js_node_public_key = cx.string(node_public_key);
-                    js_top_tier_faulty_nodes_filtered.set(&mut cx, i as u32, js_node_public_key).unwrap();
-            }
+            let js_top_tier = vec_to_js_array(&mut cx, analysis_result.top_tier.clone());
+            let js_top_tier_faulty_nodes_filtered = vec_to_js_array(&mut cx, analysis_result.top_tier_faulty_nodes_filtered.clone());
 
             js_analysis_result.set(&mut cx, "cache_hit", js_cache_hit).unwrap();
             js_analysis_result.set(&mut cx, "has_quorum_intersection", js_has_quorum_intersection).unwrap();
@@ -181,75 +138,34 @@ declare_types! {
             js_analysis_result.set(&mut cx, "top_tier_faulty_nodes_filtered", js_top_tier_faulty_nodes_filtered).unwrap();
             Ok(js_analysis_result.upcast())
         }
+
     }
+}
+
+pub fn vec_to_js_array<'a, C: Context<'a>>(cx: &mut C, vec: Vec<String>) -> Handle<'a, JsArray> {
+    let array = JsArray::new(cx, vec.len() as u32);
+    for (i, string) in vec.iter().enumerate() {
+        let js_string = cx.string(string);
+        array.set( cx, i as u32, js_string).unwrap();
+    }
+
+    array
+}
+
+
+pub fn vec_vec_to_js_array_array<'a, C: Context<'a>>(cx: &mut C, vec_vec: Vec<Vec<String>>) -> Handle<'a, JsArray> {
+    let array_array = JsArray::new(cx, vec_vec.len() as u32);
+    for (i, vec) in vec_vec.iter().enumerate() {
+        let array = vec_to_js_array(cx, vec.clone());
+        array_array.set( cx, i as u32, array).unwrap();
+    }
+
+    array_array
 }
 
 register_module!(mut cx, {
     cx.export_class::<JsFbasAnalyzer>("FbasAnalyzer")?;
     Ok(())
 });
-
-/*pub fn analyze(mut cx: FunctionContext) -> JsResult<JsObject> {
-    let nodes = cx.argument::<JsString>(0)?.value();
-    //let failingNodes = cx.argument::<JsArray>(1)?.value();
-    let fbas = Fbas::from_json_str(nodes.as_str());
-    let fbas = fbas.to_standard_form();
-    let fbas_hash = hex::encode(Sha3_256::digest(&fbas.to_json_string().into_bytes()));
-    println!(
-        "SHA3 hash of FBAS in standard form (when converted to JSON): {}",
-        fbas_hash
-    );
-
-    //'javascript' object returned to nodejs
-    let js_analysis_results = JsObject::new(&mut cx);
-    let mut results_cache: HashMap<Fbas, CustomResultsStruct> = HashMap::new();
-    // Now we only need to `do_analysis` when something significant changes in the quorum set
-    // configuration!
-    let mut is_cached = false;
-    let analysis_results = if let Some(cached_results) = results_cache.get(&fbas) {
-        println!("FBAS not updated, reuse results!");
-        is_cached = true;
-        cached_results.clone()
-    } else {
-        println!("FBAS updated, running analysis");
-        let new_results = do_analysis_meh(&fbas);
-        results_cache.insert(fbas.clone(), new_results.clone());
-        new_results
-    };
-    let js_is_cached = cx.boolean(is_cached);
-    js_analysis_results.set(&mut cx, "isCached", js_is_cached).unwrap();
-
-    let has_quorum_intersection = cx.boolean(analysis_results.has_quorum_intersection);
-
-    let minimal_blocking_sets = analysis_results.minimal_blocking_sets.clone().into_pretty_vec_vec(&fbas, None);
-    let js_minimal_blocking_sets = JsArray::new(&mut cx, minimal_blocking_sets.len() as u32);
-    for (i, minimal_blocking_set) in minimal_blocking_sets.iter().enumerate() {
-        let js_minimal_blocking_set = JsArray::new(&mut cx, minimal_blocking_set.len() as u32);
-        for (i, node_public_key) in minimal_blocking_set.iter().enumerate() {
-            let js_node_public_key = cx.string(node_public_key);
-            js_minimal_blocking_set.set(&mut cx, i as u32, js_node_public_key).unwrap();
-        }
-
-        js_minimal_blocking_sets.set(&mut cx, i as u32, js_minimal_blocking_set).unwrap();
-    }
-
-    let minimal_splitting_sets = analysis_results.minimal_splitting_sets.clone().into_pretty_vec_vec(&fbas, None);
-    let js_minimal_splitting_sets = JsArray::new(&mut cx, minimal_splitting_sets.len() as u32);
-    for (i, minimal_splitting_set) in minimal_splitting_sets.iter().enumerate() {
-        let js_minimal_splitting_set = JsArray::new(&mut cx, minimal_splitting_set.len() as u32);
-        for (i, node_public_key) in minimal_splitting_set.iter().enumerate() {
-            let js_node_public_key = cx.string(node_public_key);
-            js_minimal_splitting_set.set(&mut cx, i as u32, js_node_public_key).unwrap();
-        }
-
-        js_minimal_splitting_sets.set(&mut cx, i as u32, js_minimal_splitting_set).unwrap();
-    }
-
-    js_analysis_results.set(&mut cx, "hasQuorumIntersection", has_quorum_intersection).unwrap();
-    js_analysis_results.set(&mut cx, "minimalBlockingSets", js_minimal_blocking_sets).unwrap();
-    js_analysis_results.set(&mut cx, "minimalSplittingSets", js_minimal_splitting_sets).unwrap();
-
-    Ok(js_analysis_results)
-}*/
 
 
